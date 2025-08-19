796923d0010b6dc55f4c812f52d2598bbaf9823f
jfederico <jesus@123it.ca>
2025-08-19 17:33:08 +0000

    restore get_classes_implementing and extnded to extended_get_classes_implementing

--- public/mod/bigbluebuttonbn/classes/extension.php
+++ public/mod/bigbluebuttonbn/classes/extension.php
@@ -89,13 +89,51 @@ class extension {
     }
 
     /**
+     * Get classes are named on the base of this classname and implementing this class
+     *
+     * @param string $classname
+     * @return array
+     */
+    protected static function get_classes_implementing(string $classname): array {
+        // Get the class basename without Reflection API.
+        $classnamecomponents = explode("\\", $classname);
+        $classbasename = end($classnamecomponents);
+        $allsubs = core_plugin_manager::instance()->get_plugins_of_type(self::BBB_EXTENSION_PLUGIN_NAME);
+        $extensionclasses = [];
+        $names = core_component::get_plugin_list(self::BBB_EXTENSION_PLUGIN_NAME);
+        $sortedlist = self::get_sorted_plugins_list($names); // Make sure to use the most updated list.
+        $sortedlist = array_flip($sortedlist);
+        foreach ($allsubs as $sub) {
+            if (!$sub->is_enabled()) {
+                continue;
+            }
+            $targetclassname = "\\bbbext_{$sub->name}\\bigbluebuttonbn\\$classbasename";
+            if (!class_exists($targetclassname)) {
+                continue;
+            }
+            if (!is_subclass_of($targetclassname, $classname)) {
+                debugging("The class $targetclassname should extend $classname in the subplugin {$sub->name}. Ignoring.");
+                continue;
+            }
+            if (!isset($sortedlist[$sub->name])) {
+                debugging("The class $targetclassname does not belong to an existing subplugin. Ignoring");
+                continue;
+            }
+            // Return all extension classes based on subplugin order on manage extension page.
+            $sortorder = $sortedlist[$sub->name];
+            $extensionclasses[$sortorder] = $targetclassname;
+        }
+        return $extensionclasses;
+    }
+
+    /**
      * Get classes in enabled subplugins that extend or implement the given class/interface.
      * Tries both legacy (bigbluebuttonbn\) and modern (namespace after mod_bigbluebuttonbn\) locations.
      *
      * @param string $classname
      * @return array
      */
-    protected static function get_classes_implementing(string $classname): array {
+    protected static function extended_get_classes_implementing(string $classname): array {
         $classnamecomponents = explode("\\", $classname);
         $classbasename = end($classnamecomponents);
         // Detect subnamespace (portion after mod_bigbluebuttonbn) to support modern layout as well as legacy.
@@ -273,7 +311,7 @@ class extension {
      * @return string|null Rendered information for the instance, or null if no override found.
      */
     public static function get_rendered_output_override($renderer, $instance): ?string {
-        $classes = self::get_classes_implementing(\mod_bigbluebuttonbn\output\view_page::class);
+        $classes = self::extended_get_classes_implementing(\mod_bigbluebuttonbn\output\view_page::class);
         if (!empty($classes)) {
             $outputclass = reset($classes);
             if (class_exists($outputclass)) {
